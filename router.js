4a6fa0074b68f8f02af0e15f3723c12774a81771
Tyler Brown and Tyler Whisett <elementalplug@gmail.com>
2015-03-04 15:38:37 -0800

    Include a nested route in router and add template

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ Auction.Router.map(function() {
   this.resource('lots', {path: '/'});
   this.resource('lot', {path: ':lot_id'}, function() {
     this.resource('new-lot-item');
+    this.resource('show-item');
   });
   this.resource('new-lot');
 });
