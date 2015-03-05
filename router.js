b87358bb6f44e91f2989385fa44e3d35c920db2f
Tyler Brown and Tyler Whisett <student@epicodus.com>
2015-03-04 16:51:54 -0800

    fix nested detail printing.

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ Auction.Router.map(function() {
   this.resource('lots', {path: '/'});
   this.resource('lot', {path: ':lot_id'}, function() {
     this.resource('new-lot-item');
-    this.resource('show-item');
+    this.resource('show-item', {path:':item_id'});
   });
   this.resource('new-lot');
 });
